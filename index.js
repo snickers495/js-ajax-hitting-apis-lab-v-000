284df5b265e77d697eabb4eafe05dc81697ebd12
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:13:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function getRepositories(){
   req.open("GET", url)
   req.send()
 }
-function showRepositories(){
-  
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+function getCommits(el){
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
 }
