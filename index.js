b09a6f06c97f63ec355487fedbf310d0ad0bc728
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:19:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function getCommits(el){
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList =   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
-  document.getElementById("commits").innerHTML = commitsList;
+  document.getElementById("details").innerHTML = commitsList;
 }
