64529c8b6da29ca81093d528c638b132d09125cd
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getRepositories(){
   req.open("GET", url)
   req.send()
 }
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + ' - '+'<a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a>').join('')}</ul>`
