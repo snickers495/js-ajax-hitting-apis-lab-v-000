b5da1ba4d9c33d352b359ec0a61ae17ea80e0984
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,9 @@ function displayCommits() {
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.author.name + " - " + commit.commit.message + '</li>').join('')}</ul>`;
   document.getElementById("details").innerHTML = commitsList;
 }
+function getBranches(){
+
+}
+function displayBranches(){
+  
+}
