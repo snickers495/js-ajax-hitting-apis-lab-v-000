247234beb1098cb01da0d6abb63a1341f77e1622
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getRepositories(){
   const user = document.getElementById("username").value
   const url = "https://api.github.com/users/" + user + "/repos"
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", url)
   req.send()
 }
