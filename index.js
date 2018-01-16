7031a04a7e65cf13e1ef08377066c145c4c46697
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:25:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getRepositories(){
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + '<a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + ' - '+'<a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 function getCommits(el){
@@ -31,5 +31,5 @@ function getBranches(){
 
 }
 function displayBranches(){
-  
+
 }
