219bc3be8665032b4528934ab24c3a65f280a929
Lisa Lee <snickers495@gmail.com>
2018-01-16 21:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function showRepositories(event, data) {
 function getCommits(el){
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
+  req.addEventListener("load", displayCommits)
   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
   req.send()
 }
